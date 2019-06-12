874660c4347510d964dab0415f2502a6712a01f0
k-rathi <kunalrathi1994@gmail.com>
2019-06-12 14:04:38 -0700

    fixed some more random shit

--- src/ContentPage.jsx
+++ src/ContentPage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './ContentPage.css';
 // import AboutBody from './AboutBody';
 
-const ContentPage = ({ title, bodyContent, backgroundImg }) => (
+const ContentPage = ({
+  title, bodyContent, bodyComponent, backgroundImg,
+}) => (
   <div className="pageContainer">
     <img className="backgroundImg" src={require(`./img/${backgroundImg}`)} alt="Background" />
     <div className="pageText">
       <p className="pageTitle">{title}</p>
-      <this.props.bodyComponent bodyContent={bodyContent} />
+      <bodyComponent bodyContent={bodyContent} />
     </div>
   </div>
 );
