69b4856ed4f801897dece90a7ab8667e5933bb1d
k-rathi <kunalrathi1994@gmail.com>
2019-06-12 18:45:23 -0700

    fixed scrollable

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,33 @@
 import React, { Component, Fragment } from 'react';
-import ReactPageScroller from 'react-page-scroller';
 import './App.css';
+import { configureAnchors } from 'react-scrollable-anchor';
 import ContentPage from './ContentPage';
 import pages from './data/pages';
 import Header from './Header';
 import Seperator from './Seperator';
 import UserNav from './UserNav';
 
+configureAnchors({ scrollDuration: 1000 });
+
 
 // import SponsorsBody from './SponsorsBody';
 // import RegisterBody from './RegisterBody';
-class App extends Component {
-  goToPage = (pageNumber) => {
-    this.reactPageScroller.goToPage(pageNumber);
-  }
-
-
-  render = () => (
-    <ReactPageScroller>
-      <Header />
-      <UserNav />
-      {pages.map(page => (
-        <Fragment key={page.key}>
-          <Seperator id={page.href} />
-          <ContentPage
-            dictKey={page.key}
-            backgroundImg={page.backgroundImg}
-            title={page.pageTitle}
-            bodyContent={page.bodyContent}
-          />
-        </Fragment>
-      ))}
-    </ReactPageScroller>
-  )
-}
+const App = () => (
+  <div>
+    <Header />
+    <UserNav />
+    {pages.map(page => (
+      <Fragment key={page.key}>
+        <Seperator id={page.href} />
+        <ContentPage
+          dictKey={page.key}
+          backgroundImg={page.backgroundImg}
+          title={page.pageTitle}
+          bodyContent={page.bodyContent}
+        />
+      </Fragment>
+    ))}
+  </div>
+);
 
 export default App;
--- src/Seperator.jsx
+++ src/Seperator.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './Seperator.css';
+import ScrollableAnchor from 'react-scrollable-anchor';
 
 const Seperator = ({ id }) => (
-  <div id={id} className="seperator" />
+  <ScrollableAnchor id={id}>
+    <div className="seperator" />
+  </ScrollableAnchor>
 );
 
 export default Seperator;
