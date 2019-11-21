53d7641159efef284bb56f07c0b062d7d33722de
Jai Smith <jksmithnyc@gmail.com>
2019-11-20 21:20:19 -0500

    added code-styling to individual QA blocks

--- src/FAQBody.jsx
+++ src/FAQBody.jsx
@@ -3,27 +3,40 @@ import PropTypes from 'prop-types';
 import './FAQBody.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
+// generate function-style blocks for QA
+function styleFuncBlock(title, text) {
+  return (
+    <Container className="Code">
+      <h3 className="QATitle">
+        <span className="keyword">func </span>
+        <span className="function">{title.toLowerCase().replace(/ /g, "_")}</span>
+        <span className="syntax">() {String.fromCharCode(123)}</span>
+      </h3>
+      <p className="QAText">{text}</p>
+      <p className="syntax">{String.fromCharCode(125)}</p>
+    </Container>
+  )
+}
+
 const FAQBody = ({ bodyContent: { col1, col2 } }) => (
   <Container className="colContainer">
     <Row>
       <Col>
         {col1.map(QA => (
           <div>
-            <h3 className="QATitle">{QA.title}</h3>
-            <p className="QAText">{QA.text}</p>
+            {styleFuncBlock(QA.title, QA.text)}
           </div>
         ))}
       </Col>
       <Col>
         {col2.map(QA => (
           <div>
-            <h3 className="QATitle">{QA.title}</h3>
-            <p className="QAText">{QA.text}</p>
+            {styleFuncBlock(QA.title, QA.text)}
           </div>
         ))}
       </Col>
     </Row>
-  </Container>n
+  </Container>
 );
 FAQBody.propTypes = {
   bodyContent: PropTypes.shape({
