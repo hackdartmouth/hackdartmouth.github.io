9a53896e83b772bc08a294fa3c05c5a063dcfef8
Jai Smith <jksmithnyc@gmail.com>
2020-01-19 17:37:43 -0500

    fixed sideways scrolling

--- src/Header.jsx
+++ src/Header.jsx
@@ -14,58 +14,56 @@ const headerInfo = {
 const Header = () => (
   <>
     <div className="headerContainer" style={{ backgroundColor: headerInfo.sectionColor }}>
-	<div style={{position: "absolute"}}>
-	<Particles 
-	height="100vh"
-	width="100vw"
-    params={{
-	    "particles": {
-	        "number": {
-	            "value": 160,
-	            "density": {
-	                "enable": false
-	            }
-	        },
-	        "size": {
-	            "value": 10,
-	            "random": true
-	        },
-	        "move": {
-	            "direction": "bottom",
-	            "out_mode": "out"
-	        },
-	        "line_linked": {
-	            "enable": false
-	        }
-	    },
-	    "interactivity": {
-	        "events": {
-	            "hover": {
-	                "enable": false,
-	                "mode": "repulse"
-	            }
-	        },
-	        "modes": {
-	            "remove": {
-	                "particles_nb": 10
-	            }
-	        }
-	    }
-	}}/>
-	</div>
-	<div className="textBigContainer">
-		<div className="textContainer">
-			<p className="title">{headerInfo.title}</p>
-			<p className="info">{`${headerInfo.date} | ${headerInfo.location}`}</p>
-			<Link activeClass="active" className="test1" to={"register"} spy={true} smooth={true} duration={500} offset={-58}>
-				<Button variant="outline-light" className="registerButton" style={{ backgroundColor: "#ffffff", borderStyle: "none", color: "#5f936a", fontWeight: "bold"}}>
-				Register!
-				</Button>
-			</Link>
+		<div style={{ position: "absolute", width: '100%' }}>
+			<Particles 
+				height='100vh'
+				width='100%'
+				params={{
+					"particles": {
+						"number": {
+							"value": 160,
+							"density": {
+								"enable": true
+							}
+						},
+						"size": {
+							"value": 10,
+							"random": true
+						},
+						"move": {
+							"direction": "bottom",
+							"out_mode": "out"
+						},
+						"line_linked": {
+							"enable": false
+						}
+					},
+					"interactivity": {
+						"events": {
+							"hover": {
+								"enable": false,
+								"mode": "repulse"
+							}
+						},
+						"modes": {
+							"remove": {
+								"particles_nb": 10
+							}
+						}
+					}
+				}}/>
 		</div>
-	</div>
-	
-    
+		<div className="textBigContainer">
+			<div className="textContainer">
+				<p className="title">{headerInfo.title}</p>
+				<p className="info">{`${headerInfo.date} | ${headerInfo.location}`}</p>
+				<Link activeClass="active" className="test1" to={"register"} spy={true} smooth={true} duration={500} offset={-58}>
+					<Button variant="outline-light" className="registerButton" style={{ backgroundColor: "#ffffff", borderStyle: "none", color: "#5f936a", fontWeight: "bold"}}>
+					Register!
+					</Button>
+				</Link>
+			</div>
+		</div>
     </div>
   </>
  
