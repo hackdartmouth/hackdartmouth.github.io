29b19929f4f016c772f298c145cd13ff2a58396b
Jai Smith <jksmithnyc@gmail.com>
2020-01-22 22:22:09 -0500

    removed react-document-title, set directly in App.jsx constructor (which is now a class)

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,36 @@ import pages from './data/pages';
 import Header from './Header';
 import UserNav from './UserNav';
 import Favicon from 'react-favicon';
-import DocumentTitle from 'react-document-title';
 
 configureAnchors({ scrollDuration: 250 });
 
-const App = () => (
-  <div>
-    <DocumentTitle title='HackDartmouth' />
-    <Favicon url={require(`./img/favicon.ico`)} />
-    <Header />
-    <UserNav />
-    {pages.map((page,i) => (
-      <Fragment key={page.key}>
-        <ContentPage
-          dictKey={page.key}
-          sectionColor={page.sectionColor}
-          title={page.pageTitle}
-          titleColor={page.titleColor}
-          bodyContent={page.bodyContent}
-          href={page.href}
-          first={i==0}
-          last={i==pages.length-1}
-        />
-      </Fragment>
-    ))}
-  </div>
-);
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    document.title = 'HackDartmouth'
+  }
 
-export default App;
+  render() {
+    return (
+      <div>
+        <Favicon url={require(`./img/favicon.ico`)} />
+        <Header />
+        <UserNav />
+        {pages.map((page,i) => (
+          <Fragment key={page.key}>
+            <ContentPage
+              dictKey={page.key}
+              sectionColor={page.sectionColor}
+              title={page.pageTitle}
+              titleColor={page.titleColor}
+              bodyContent={page.bodyContent}
+              href={page.href}
+              first={i==0}
+              last={i==pages.length-1}
+            />
+          </Fragment>
+        ))}
+      </div>
+    )
+  }
+}
