23aa2ffe70317f5ffa41c4f651d73bb5a77d72a6
fitwit <fitwit@Lories-MacBook-Pro.local>
2020-02-26 23:46:57 -0500

    mlh logo

--- src/UserNav.jsx
+++ src/UserNav.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './UserNav.css';
 import { Navbar, Nav } from 'react-bootstrap';
 import pages from './data/pages';
@@ -19,6 +19,7 @@ class UserNav extends Component {
     render = () => {
       const { brandImg } = this.state;
       return (
+        <Fragment>
         <Navbar collapseOnSelect sticky="top" bg="light" expand="lg">
           <a onClick={() => animateScroll.scrollToTop()}>
           <Navbar.Brand><img height="30" className="brandImg" src={require(`./img/pages/${brandImg}`)} alt="brand" /></Navbar.Brand>
@@ -30,6 +31,8 @@ class UserNav extends Component {
             </Nav>
           </Navbar.Collapse>
         </Navbar>
+        <a id="mlh-trust-badge" style={{display:"block",maxWidth:"100px", minWidth:"60px", position:"fixed", right:"75px", top:"0", width:"10%", zIndex:"10000"}} href="https://mlh.io/seasons/na-2020/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2020-season&utm_content=gray" target="_blank"><img src="https://s3.amazonaws.com/logged-assets/trust-badge/2020/mlh-trust-badge-2020-gray.svg" alt="Major League Hacking 2020 Hackathon Season" style={{width:"100%"}}/></a>
+        </Fragment>
       );
     }
 }
