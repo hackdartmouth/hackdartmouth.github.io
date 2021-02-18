e10d4335e879f477a90768859652f35c96423011
Jai <jksmithnyc@gmail.com>
2021-02-17 20:16:08 -0600

    re-enable registration

--- src/RegisterBody.jsx
+++ src/RegisterBody.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import './RegisterBody.css';
 import * as typeformEmbed from "@typeform/embed";
 import { isMobile } from 'react-device-detect';
-import { SocialIcon } from 'react-social-icons';
 
 class RegisterBody extends React.Component {
   constructor(props) {
@@ -16,7 +15,7 @@ class RegisterBody extends React.Component {
       typeformEmbed.makeWidget(this.form, this.props.bodyContent.typeformUrl, {
         hideFooter: true,
         hideHeaders: true,
-        opacity: 0
+        opacity: 0,
       });
     }
   }
@@ -24,7 +23,7 @@ class RegisterBody extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="registerSubTitle">{`${this.props.bodyContent.date} | ${this.props.bodyContent.location}`}</h1>
+        <h1 className="registerSubTitle">{this.props.bodyContent.date}</h1>
         <p className="registerDetails">{this.props.bodyContent.info}</p>
         <div ref={(form) => this.form = form} style={{width: '100%', height: isMobile ? '10em' : '20em', marginTop: '1em'}} />
       </div>
