405731854adff3b583b583d8b48e6b7ed04df713
Jai <jksmithnyc@gmail.com>
2021-04-15 20:27:45 -0400

    reopen registration

--- src/RegisterBody.jsx
+++ src/RegisterBody.jsx
@@ -10,22 +10,22 @@ class RegisterBody extends React.Component {
     this.form = null;
   }
 
-  // componentDidMount() {
-  //   if (this.form) {
-  //     typeformEmbed.makeWidget(this.form, this.props.bodyContent.typeformUrl, {
-  //       hideFooter: true,
-  //       hideHeaders: true,
-  //       opacity: 0,
-  //     });
-  //   }
-  // }
+  componentDidMount() {
+    if (this.form) {
+      typeformEmbed.makeWidget(this.form, this.props.bodyContent.typeformUrl, {
+        hideFooter: true,
+        hideHeaders: true,
+        opacity: 0,
+      });
+    }
+  }
 
   render() {
     return (
       <div>
         <h1 className="registerSubTitle">{this.props.bodyContent.date}</h1>
         <p className="registerDetails">{this.props.bodyContent.info}</p>
-        {/* <div ref={(form) => this.form = form} style={{width: '100%', height: isMobile ? '10em' : '20em', marginTop: '1em'}} /> */}
+        <div ref={(form) => this.form = form} style={{width: '100%', height: isMobile ? '10em' : '20em', marginTop: '1em'}} />
       </div>
     );
   }
