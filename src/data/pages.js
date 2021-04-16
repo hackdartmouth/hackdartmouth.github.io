790acf63dd60fef1d1c2578caf78331c83c33e77
Jai <jksmithnyc@gmail.com>
2021-04-15 20:30:20 -0400

    registration message tweak

--- src/data/pages.js
+++ src/data/pages.js
@@ -190,7 +190,7 @@ const pages = [
       event: 'HackDartmouth VIrtual',
       date: 'April 16-18th, 2021',
       // location: 'Virtual',
-      info: 'Registration for non-Dartmouth students closes March 20th. Dartmouth students may register up to the day of the hackathon',
+      info: 'Registration for non-Dartmouth students closes March 20th. Dartmouth students may register until April 16th at 10 am EDT.',
       typeformUrl: 'https://hackdartmouth.typeform.com/to/LU7ubUtU',
     },
     key: 'RegisterBody',
